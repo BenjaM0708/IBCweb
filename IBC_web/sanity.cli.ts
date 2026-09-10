1fa9028a3ad50a9184d31e4fd0e6386760bd9ce9
Benja Muñoz <benjamunozja@gmail.com>
2026-09-10 02:42:17 +0200

    Web Studio Deployed

--- IBC_web/sanity.cli.ts
+++ IBC_web/sanity.cli.ts
@@ -4,5 +4,9 @@ export default defineCliConfig({
   api: {
     projectId: 'idytbnwx',  
     dataset: 'production'  
-  }
+  },
+  deployment: {
+    appId: 'q1gvbdrwsagrd6o18msf09xg'
+  },
+  autoUpdates: true
 })
